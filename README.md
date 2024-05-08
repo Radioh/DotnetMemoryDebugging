4bfc6e7ce0d00fbd852a81463af4625d27881a6e
Jeppe Ærenlund <jeppe@aerenlund.dev>
2024-05-08 11:04:05 +0200

    Add memory leak example

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# DotnetMemoryDebugging
-.Net memory debugging example
+# Advanced .NET Memory Debugging
+
+## Description
+This project is a simple example of how to debug memory leaks in a .NET application. 
+
+## Example: Memory Leak
+Demonstrates a scenario where a list of objects is continuously populated without being cleared, leading to a potential memory leak.
+
+## Example: Thread pool starvation (TBD)
+Demonstrates a scenario where a thread pool is starved due to a long running task.
+
+## Example: Blocked finalizer queue (TBD)
+Demonstrates a scenario where the finalizer queue is blocked due to a long running finalizer.
+
+## Getting Started
+To run the examples, simply clone the repository and run the projects.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
---
+++ Source/MemoryLeak/Program.cs
@@ -0,0 +1,37 @@
+﻿// Simple program that leaks memory by creating a list of objects that are never released.
+
+var persons = new List<Person>();
+var exit = false;
+
+while (!exit)
+{
+    foreach (var i in Enumerable.Range(0, 10_000))
+    {
+        persons.Add(new Person(
+            id: persons.Count,
+            firstName: $"John #{persons.Count}",
+            lastName: "Doe"));
+    }
+
+    Console.WriteLine("Leak more memory (y/n)");
+    var answer = Console.ReadLine();
+    exit = answer != "y";
+}
+
+Console.WriteLine("Bye!");
+
+internal class Person
+{
+    internal Person(int id, string firstName, string lastName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public int Id { get; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    private char[] Details { get; } = new char[10_000];
+}
+
